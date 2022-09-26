b84cb5d467982384ba12729d8761f0496a430a9f
DESKTOP-G0KKLD0\rsc <15731416521@163.com>
2022-09-26 14:18:55 +0800

    chore: 全局scss变量

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,41 @@
 import {fileURLToPath, URL} from 'node:url'
 
-import {defineConfig} from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(), vueJsx()
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server: { // 使用本机ip或者localhost启动服务
-    host: '0.0.0.0',
-    cors: true, // 默认启用并允许任何源
-    open: true,
-    // 在服务器启动时自动在浏览器中打开应用程序
-    // port: 2180,
-    // proxy: {
-    // '/api': {
-    //     target: config.VITE_APP_API_URL,
-    //     changeOrigin: true,
-    //     rewrite: (path) => path.replace(/^\/api/, '')
-    // }
-    // }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: '@use "./src/assets/variable.scss" as *;'
+export default defineConfig((({mode}) => {
+  const config = loadEnv(mode, './')
+  return {
+    plugins: [
+      vue(), vueJsx()
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: { // 使用本机ip或者localhost启动服务
+      host: '0.0.0.0',
+      cors: true, // 默认启用并允许任何源
+      open: true,
+      // 在服务器启动时自动在浏览器中打开应用程序
+      // port: 2180,
+      // proxy: {
+      // '/api': {
+      //     target: config.VITE_APP_API_URL,
+      //     changeOrigin: true,
+      //     rewrite: (path) => path.replace(/^\/api/, '')
+      // }
+      // }
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "./src/assets/variable.scss" as *;'
+        }
       }
     }
   }
-})
+}))
