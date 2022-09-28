737f850ae7ebccd828a0b4a16ce24bcfe243ee86
DESKTOP-G0KKLD0\rsc <15731416521@163.com>
2022-09-28 15:10:05 +0800

    readme

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import {fileURLToPath, URL} from 'node:url'
-
+import path from 'path'
 import {defineConfig, loadEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
@@ -12,6 +12,7 @@ export default defineConfig(({mode}) => {
     plugins: [
       vue(), vueJsx()
     ],
+    base: path.resolve(__dirname, './dist/'),
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url))
