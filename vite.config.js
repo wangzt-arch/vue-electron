22590c2132c6fed263ea95b6c7767227f327708b
DESKTOP-G0KKLD0\rsc <15731416521@163.com>
2022-09-28 15:53:13 +0800

    readme

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,19 @@
-import {fileURLToPath, URL} from 'node:url'
-import path from 'path'
-import {defineConfig, loadEnv} from 'vite'
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
-export default defineConfig(({mode}) => {
+export default defineConfig(({ mode }) => {
   const config = loadEnv(mode, './')
   console.log(config);
   return {
     plugins: [
       vue(), vueJsx()
     ],
-    base: path.resolve(__dirname, './dist/'),
+    // base: path.resolve(__dirname, './dist/'),
+    base: './',
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -21,7 +22,8 @@ export default defineConfig(({mode}) => {
     server: { // 使用本机ip或者localhost启动服务
       host: '0.0.0.0',
       cors: true, // 默认启用并允许任何源
-      open: true,
+      open: false,
+      port: 5173,
       // 在服务器启动时自动在浏览器中打开应用程序
       // port: 2180,
       // proxy: {
